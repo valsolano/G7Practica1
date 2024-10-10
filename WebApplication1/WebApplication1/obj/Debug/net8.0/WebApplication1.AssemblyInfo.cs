659788a5d699cdf0fd23681fdcdf19119b013ba8
valsv0602 <valeriasv0602@gmail.com>
2024-10-10 15:51:09 -0600

    paginas joan y jose

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4409b6aa64865d45fe6f62b9ed1b86e7d0bfaf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0d34f1a226616d0916a47303346b4452d13dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
